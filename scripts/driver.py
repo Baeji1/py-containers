d0dff62f4cc5ea521d0846db05ff51960b6c31c6
Rajat <rajats643@gmail.com>
2024-11-15 03:21:38 -0800

    init

---
+++ scripts/driver.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+
+def main():
+    print('done')
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+1. Arrays
+2. Linked Lists
+3. Stacks
+4. Queues
+5. Maps & Hash Tables
+6. Graphs
+7. Trees
+8. Binary Trees & Binary Search Trees
+9. Self-balancing Trees (AVL Trees, Red-Black Trees, Splay Trees)
+10. Heaps
+11. Tries
+12. Segment Trees
+13. Fenwick Trees
+14. Disjoint Set Union
+15. Minimum Spanning Trees
+ropes
+fibonacci trees
+b trees
+
+"""
